fe25b6068d2a5ccaf979e6009203afb2f4667c36
Robson Ferreira <tech.rob@icloud.com>
2021-05-21 20:02:23 -0400

    second commit

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const usersRoute = require('./routes/UserRoutes')
+const port = require('./index')
+const ejs = require('ejs')
+const passport = require('passport')
+const User = require('./model/User')
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session')
+const FileStore = require('session-file-store')(session)
+const { v4: uuidv4 } = require('uuid');
 
 const app = express()
 
@@ -8,6 +16,15 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/users', usersRoute)
+app.set('view engine', 'ejs')
+app.use(passport.initialize())
+app.use(passport.session())
+
+
+// serving static files
+// app.use('/images', express_static(__dirname + '/images'))
+// app.use('/css', express_static(__dirname + '/css'))
+// app.use('/fonts', express_static(__dirname + '/fonts'))
 
 const MONGO_URI = "mongodb+srv://dbUser:1029384756@cluster1.gskfs.mongodb.net/UserAuthDB?retryWrites=true&w=majority"
 mongoose.connect(MONGO_URI, 
@@ -17,7 +34,42 @@ mongoose.connect(MONGO_URI,
 
 app.get('/', (req, res) => res.send('this is a test'))
 
-const port = process.env.PORT || 4000
-app.listen(port, () => console.log(`Server is listening at localhost:${port}`))
+passport.use(new LocalStrategy(
+	{usernameField: 'userName'},
+	(userName, password, done) => {
+		User.findOne({userName}, (err, userData) => {
+			let passwordCheck = bcrypt.compareSync(password,
+				userData.password)
+				if (userName === userData.userName && passwordCheck){
+					return done(null, userData)
+				}
+		})
+	}
+))
+app.post('/login', (req, res, next) => {
+	passport.authenticate('local', (err, user, info) => {
+		req.login(user, (err) => {
+			res.redirect('/index')
+		})
+	})(req, res, next)
+})
 
+app.use(session({
+	genid: (req) => {
+	return uuid() // use UUIDs for session IDs
+	},
+	store: new FileStore(),
+	secret: 'anything is okay',
+	resave: false,
+	saveUninitialized: true
+ }))
+ passport.serializeUser((user, done) => {
+	 done(null, user.id);
+ });
+ passport.deserializeUser(function(id, done) {
+ User.findById(id, function(err, user) { 
+	 loggedInUser = user;
+	 done(err, user);
+ });
+ });
 
--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -2,6 +2,12 @@ const express = require('express')
 const router = express.Router()
 const User = require('../model/User')
 const bcrypt = require('bcrypt')
+const ejs = require('ejs')
+const { response } = require('express')
+const passport = require('passport')
+
+// app.set('view engine', 'ejs')
+
 
 router.post('/user', (req, res) => {
 	const user = new User({
@@ -11,6 +17,14 @@ router.post('/user', (req, res) => {
 	password: req.body.password,
 	email: req.body.email
     })
+
+router.get('/', (req, res) => res.sendFile(path.join(__dirname+'/views/signup.html')))
+
+router.get('/users', (req, res) => {
+    User.find({} , (error, result) => result ? response.render('availableUsers', {'users': result}) : res.status(404))
+})
+
+
 
 
 bcrypt.hash(user.password, 10, (err, hash) => {
